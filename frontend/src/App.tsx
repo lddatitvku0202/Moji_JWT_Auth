c5c3824894a6afa960d3c6a75205658363ec77b0
THUONGVIP09 <thuonglth.itb@vku.udn.vn>
2025-12-01 07:18:51 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignInPage from "./pages/SignInPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+import SignUpPage from "./pages/SignUpPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { useThemeStore } from "./stores/useThemeStore";
+import { useEffect } from "react";
+
+function App() {
+  const { isDark, setTheme } = useThemeStore();
+
+  useEffect(() => {
+    setTheme(isDark);
+  }, [isDark]);
+
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          {/* public routes */}
+          <Route
+            path="/signin"
+            element={<SignInPage />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUpPage />}
+          />
+
+          {/* protectect routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route
+              path="/"
+              element={<ChatAppPage />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
